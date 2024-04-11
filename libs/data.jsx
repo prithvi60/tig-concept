3374d8ffbeaaf48fb22b8bda1274532bec5d1dab
Prithvi <prithvin19@gmail.com>
2024-04-11 10:17:13 +0530

    t

--- libs/data.jsx
+++ libs/data.jsx
@@ -1,41 +1,41 @@
 // export const companies = ["tigital", "tigx", "tigpods", "ticreators", "tigom"];
 
 export const companies = [
-  { id: 1, title: "tigital", hoverImg: "/bg 1.png", href: "#" },
-  { id: 2, title: "tigx", hoverImg: "/bg 2.png", href: "#" },
-  { id: 3, title: "tigpods", hoverImg: "/bg 3.png", href: "#" },
-  { id: 4, title: "ticreators", hoverImg: "/bg 1.png", href: "#" },
-  { id: 5, title: "tigom", hoverImg: "/bg 3.png", href: "#" },
+  { id: 1, title: "TiGital", hoverImg: "/bg 1.png", href: "#" },
+  { id: 2, title: "TigX", hoverImg: "/bg 2.png", href: "#" },
+  { id: 3, title: "TigPods", hoverImg: "/bg 3.png", href: "#" },
+  { id: 4, title: "CreatorsUnion", hoverImg: "/bg 1.png", href: "#" },
+  { id: 5, title: "TigOm", hoverImg: "/bg 3.png", href: "#" },
 ];
 
 export const companyLists = [
   {
     id: 1,
-    companyName: "tigital",
+    companyName: "TiGital",
     desc: "Empower your brand with our dynamic digital marketing solutions. From SEO to social media management, we drive results that elevate your online presence. Let us amplify your digital footprint today.",
     img: "/bg 1.png",
   },
   {
     id: 2,
-    companyName: "tigx",
+    companyName: "TigX",
     desc: "Empower your brand with our dynamic digital marketing solutions. From SEO to social media management, we drive results that elevate your online presence. Let us amplify your digital footprint today.",
     img: "/bg 2.png",
   },
   {
     id: 3,
-    companyName: "tigpods",
+    companyName: "TigPods",
     desc: "Empower your brand with our dynamic digital marketing solutions. From SEO to social media management, we drive results that elevate your online presence. Let us amplify your digital footprint today.",
     img: "/bg 3.png",
   },
   {
     id: 4,
-    companyName: "ticreators",
+    companyName: "CreatorsUnion",
     desc: "Empower your brand with our dynamic digital marketing solutions. From SEO to social media management, we drive results that elevate your online presence. Let us amplify your digital footprint today.",
     img: "/bg 1.png",
   },
   {
     id: 5,
-    companyName: "tigom",
+    companyName: "TigOm",
     desc: "Empower your brand with our dynamic digital marketing solutions. From SEO to social media management, we drive results that elevate your online presence. Let us amplify your digital footprint today.",
     img: "/bg 2.png",
   },
