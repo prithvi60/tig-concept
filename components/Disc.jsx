773dd76ac19137af34492a80c61ea8102b9624a8
unknown <gokulgandhi97@gmail.com>
2024-05-07 17:14:15 +0530

    changes

--- components/Disc.jsx
+++ components/Disc.jsx
@@ -25,6 +25,30 @@ const Disc = () => {
   const [progressValue, setProgressValue] = useState(0);
   // const [mute, setMute] = useState(true);
   const router = useRouter();
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+  const [cursorPositionForMobile, setCursorPositionForMobile] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  // shadow cursor Effect
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setCursorPosition({ x: event.clientX, y: event.clientY });
+    };
+    const handleTouchMove = (event) => {
+      const touch = event.touches[0];
+      setCursorPositionForMobile({ x: touch.clientX, y: touch.clientY });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("touchmove", handleTouchMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("touchmove", handleTouchMove);
+    };
+  }, []);
 
   // const [play, { stop }] = useSound(
   //   "https://ik.imagekit.io/webibee/tig-intro.mp3",
@@ -104,17 +128,19 @@ const Disc = () => {
     };
     // console.log(dynamicRoute);
   }, [router, scrollDown, dynamicRoute]);
+
   useEffect(() => {
     section === 0
-      ? setBrand(`${companies[0].title.toLowerCase()}`)
-      : section === 1
-      ? setBrand(`${companies[1].title.toLowerCase()}`)
-      : section === 2
-      ? setBrand(`${companies[2].title.toLowerCase()}`)
-      : section === 3
-      ? setBrand(`${companies[3].title.toLowerCase()}`)
-      : setBrand(`${companies[0].title.toLowerCase()}`);
+    ? setBrand(`${companies[0].title.toLowerCase()}`)
+    : section === 1
+    ? setBrand(`${companies[1].title.toLowerCase()}`)
+    : section === 2
+    ? setBrand(`${companies[2].title.toLowerCase()}`)
+    : section === 3
+    ? setBrand(`${companies[3].title.toLowerCase()}`)
+    : setBrand(`${companies[0].title.toLowerCase()}`);
   }, [section]);
+
   return (
     <section
       style={{
@@ -131,7 +157,7 @@ const Disc = () => {
             ? "linear-gradient(to bottom right, #ff00ff, #00ffff)"
             : "initial",
       }}
-      className="relative z-20 flex flex-col items-center justify-center w-full space-y-3 overflow-hidden h-dvh md:gap-6 xl:gap-8 select-none"
+      className="relative z-20 flex flex-col items-center justify-center w-full pt-20 space-y-3 overflow-hidden select-none md:pt-6 h-dvh md:gap-6 xl:gap-8"
     >
       {/* Mouse Move Effect */}
       {/* <MouseImageTrail
@@ -142,32 +168,57 @@ const Disc = () => {
       ></MouseImageTrail> */}
 
       {/* Main Company Name */}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 0.3 }}
+        transition={{ duration: 1.2, delay: 0.5, ease: "backOut" }}
+        style={{
+          top: cursorPosition.y || cursorPositionForMobile.y,
+          left: cursorPosition.x || cursorPositionForMobile.x,
+        }}
+        className="absolute -translate-x-1/2 -translate-y-1/2 bg-white rounded-full w-36 h-36 md:w-72 md:h-72 opacity-20 blur-md -z-10"
+      ></motion.div>
+      <div className="flex items-center justify-center gap-4 px-5 py-3 bg-black rounded-full shadow-sm shadow-white md:gap-10 md:px-10 md:py-3">
+        {companies.map((list) => (
+          <h5 className="text-sm text-white md:text-lg" key={list.id}>
+            {list.title}
+          </h5>
+        ))}
+      </div>
       <AnimatePresence>
         <motion.div
           initial={{ y: 150, opacity: 0 }}
           animate={{ opacity: 1, y: 0 }}
           exit={{ y: -150, opacity: 0 }}
           transition={{ duration: 1, ease: "backInOut", delay: 0.25 }}
-          className={`mt-24 h-auto tracking-wide text-red-600 capitalize font-tiltNeon text-2xl md:text-4xl xl:text-6xl text-center cursor-pointer px-4 ${
+          className={`mt-24 h-auto tracking-wide text-red-600 capitalize font-tiltNeon text-2xl md:text-4xl xl:text-6xl text-center  px-4 ${
             !scrollDown ? "block" : "hidden"
           }`}
         >
           <BubbleText value={"The Internet Generation"} />
-          <p className="text-sm text-white">
+          <p className="mx-8 text-sm text-white md:mx-36">
             Empower your brand with our dynamic digital marketing solutions.
             From SEO to social media management, we drive results that elevate
             your online presence. Let us amplify your digital footprint today.
             Empower your brand with our dynamic digital marketing solutions.
           </p>
 
-          <button
-            className={`text-xs md:text-base px-4 py-2 rounded-xl ${"bg-red-600 text-white capitalize"}`}
+          <motion.button
+            key={section}
+            initial={{ scale: 1.4, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{ duration: 1.2, ease: "easeInOut", type: "spring", stiffness: 200 }}
             onClick={() => {
               router.push(`/${brand}`);
             }}
           >
-            Visit {brand}
-          </button>
+            <h4
+              className={`text-xs md:text-base px-4 py-2 rounded-xl cursor-pointer hover:bg-black hover:text-white hover:scale-110 hover:px-6  ${"bg-red-600 text-white capitalize"}`}
+            >
+              Visit {brand}
+            </h4>
+          </motion.button>
+
           {/* {dynamicRoute === "" && (
             // <AnimatePresence>
 
@@ -196,7 +247,7 @@ const Disc = () => {
       </AnimatePresence>
 
       {/* 3D Disc Component makwe responsive */}
-      <div id="threed" className=" w-screen h-screen">
+      <div id="threed" className="w-screen h-screen ">
         <MotionConfig
           transition={{
             ...framerMotionConfig,
@@ -210,14 +261,6 @@ const Disc = () => {
             }}
           >
             <Experience section={section} setSection={setSection} />
-            {/* <ScrollControls pages={4} damping={0.1}>
-              <ScrollManager section={section} onSectionChange={setSection} />
-              <Scroll>
-              </Scroll>
-              {/* <Scroll html>
-              <Interface />
-            </Scroll> */}
-            {/* </ScrollControls> */}
           </Canvas>
         </MotionConfig>
         <Leva hidden />
